d0f1d1d29c735e8857148a69a29c1dd51de38fa7
runnedrunner <auszhou@nuevaschool.org>
2016-12-02 08:41:53 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ var allDivsInTheEditor = editor.find("div")
 editor.find("div")
 
 
+
+
+
+
+
+//seizure.js
+
+var random255 = function () {
+  return Math.floor(Math.random()*255);
+}
+var randomColor = function () {
+  return "rgb(" + random255() + "," + random255() + "," + random255() + ")";
+}
+
+
+
+var i=0;
+var intervalId = setInterval(function () {
+  if (i > 10000) {
+    clearInterval(intervalId);
+  }
+  doWork();
+  i++;
+},1);
+
+
 // Now try to
 // 1. add a button to the topbar
 // 2. change the title above the file list
